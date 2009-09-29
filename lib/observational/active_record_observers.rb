bc507d8c15798eb80032bca65f6a528e3cebb856
Brian Terlson <brian.terlson@gmail.com>
2009-09-29 18:32:07 -0500

    Rewrite active record integration, vastly simplifying the code and allowing this library to play nicely with other libraries that change the behavior of observers to take arbitrary arguments other than self.
    Prior to this change, observable made the assumption that the argument passed to the callback it used to hook into AR
    events would always be passed an instance of the object it was observing. This is not the case when some libraries
    choose to override this behavior of Rails, such as AttachmentFu. This commit fixes the incompatibility, and also vastly
    simplifies the AR integration. This also fixes a bug I didn't track down where my callbacks were being called twice due
    to Observable inserting two callbacks for an AR event.

--- lib/observational/active_record_observers.rb
+++ lib/observational/active_record_observers.rb
@@ -1,25 +1,8 @@
 module Observational
   module ActiveRecordObservers
-    def self.included(klass)
-      ActiveRecord::Callbacks::CALLBACKS.each do |callback|
-        klass.send(callback) { |obj| obj.send :notify_observers, callback }
-      end
-      klass.class_eval do
-        extend ClassMethods
-        class << self
-          alias_method_chain :define_callbacks, :observational
-        end
-      end
-    end
-
-    module ClassMethods
-      def define_callbacks_with_observational(*callbacks)
-        define_callbacks_without_observational *callbacks
-        callbacks.each do |c|
-          send(c) { |obj| obj.send :notify_observers, c }
-        end
-      end
+    def run_callbacks(kind, options = {}, &block)
+      self.send(:notify_observers, kind)
+      super
     end
   end
 end
-
